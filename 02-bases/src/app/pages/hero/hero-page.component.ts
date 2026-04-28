f27113e2a0ec78b8a8c76025e1b9e5af41a24e13
David M <davidmvecino@gmail.com>
2026-04-28 11:52:22 +0200

    fin sección: - señales computadas - pipes

--- 02-bases/src/app/pages/hero/hero-page.component.ts
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -1,19 +1,20 @@
-import {Component, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {UpperCasePipe} from '@angular/common';
 
 @Component({
   templateUrl: "./hero-page.component.html",
-  styleUrls: ['./hero-page.component.css']
+  styleUrls: ['./hero-page.component.css'],
+  imports: [UpperCasePipe]
 })
 export class HeroPageComponent {
   name = signal("Iron Man")
   age = signal(45);
 
-  getName(){
-    return `${this.name()}`;
-  }
-  getAge(){
-    return `${this.age()}`;
-  }
+  heroDescription = computed(
+    () => `${this.name()} ${this.age()}`
+  )
+  capitalizedName = computed(() => `${this.name().toUpperCase()}`);
+
   changeHero(){
     this.name.update(_ =>"Spiderman")
     this.age.update(_ => 20)
@@ -22,9 +23,6 @@ export class HeroPageComponent {
     this.name.update(_ =>"Iron man")
     this.age.update(_ => 45)
 
-  }
-  getHeroDescription(){
-    return `${this.name()} + ": " + ${this.age()}`;
   }
   changeAge(){
     this.age.update(_ => 60)
