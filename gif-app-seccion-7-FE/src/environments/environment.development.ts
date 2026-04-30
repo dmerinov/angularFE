a63822c03902985498ce2f392360e1b97a69aa6b
David M <davidmvecino@gmail.com>
2026-04-30 10:31:53 +0200

    deleted api key

--- gif-app-seccion-7-FE/src/environments/environment.development.ts
+++ gif-app-seccion-7-FE/src/environments/environment.development.ts
@@ -6,6 +6,6 @@ export const environment = {
 
   // ApiKeys
   // https://developers.giphy.com/dashboard/
-  giphyApiKey: 'gk94aLGrTB1kEtWBy5P8FgpPsc4Shusf',
+  giphyApiKey: '',
   giphyUrl: 'https://api.giphy.com/v1',
 };
